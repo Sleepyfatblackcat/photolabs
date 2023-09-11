59cec83069e823d2d9ea11bda1f018e5e5de8737
Jack Tran <bluestar113311@gmail.com>
2023-09-11 09:38:15 -0700

    implement favorite button

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
+  const [select, setSelect] = useState(false);
   return (
     <div key={props.key} className="photo-list__item">
+      <PhotoFavButton selected={select} displayAlert={select} onClick={()=>setSelect(select => !select)}/>
       <img src={props.image} alt="photo" className="photo-list__image"/>
       <div className="photo-list__user-details">
         <img src={props.profile} alt="profile picture" className="photo-list__user-profile"/>
