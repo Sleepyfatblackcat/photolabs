618f839414ad4b1219d3f1838692b20db0a9e02b
Jack Tran <bluestar113311@gmail.com>
2023-09-12 12:17:45 -0700

    refactor to use mock data

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import '../styles/HomeRoute.scss';
 import PhotoList from 'components/PhotoList';
 import TopNavigationBar from 'components/TopNavigationBar';
+import photos from '../mocks/photos';
+import topics from '../mocks/topics';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      <TopNavigationBar/>
-      <PhotoList/>
+      <TopNavigationBar topics={topics}/>
+      <PhotoList photos={photos}/>
     </div>
   );
 };
