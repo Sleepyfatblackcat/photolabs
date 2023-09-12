6ff9b399f95df3fe7ed020fe8b8c630d8cfc09ee
Jack Tran <bluestar113311@gmail.com>
2023-09-12 11:39:49 -0700

    implement photo list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,30 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import PhotoList from 'components/PhotoList';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const addPhotosToList = (photos) => {
-  const list = new Array(3);
-  for (let x = 0; x < photos; x++) {
-    list.push (
-      <PhotoListItem key={sampleDataForPhotoListItem.id}
-                     image={sampleDataForPhotoListItem.imageSource}
-                     profile={sampleDataForPhotoListItem.profile}
-                     username={sampleDataForPhotoListItem.username}
-                     city={sampleDataForPhotoListItem.location.city}
-                     country={sampleDataForPhotoListItem.location.country}
-      />
-    );
-  }
-  return list;
-}
+// const addPhotosToList = (photos) => {
+//   const list = new Array(3);
+//   for (let x = 0; x < photos; x++) {
+//     list.push (
+//       <PhotoListItem key={sampleDataForPhotoListItem.id}
+//                      image={sampleDataForPhotoListItem.imageSource}
+//                      profile={sampleDataForPhotoListItem.profile}
+//                      username={sampleDataForPhotoListItem.username}
+//                      city={sampleDataForPhotoListItem.location.city}
+//                      country={sampleDataForPhotoListItem.location.country}
+//       />
+//     );
+//   }
+//   return list;
+// }
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {addPhotosToList(3)}
+      <PhotoList/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,18 +3,18 @@ import React, {useState} from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-const PhotoListItem = (props) => {
+const PhotoListItem = ({image, profile, username, city, country}) => {
   const [select, setSelect] = useState(false);
   return (
-    <div key={props.key} className="photo-list__item">
+    <div className="photo-list__item">
       <PhotoFavButton selected={select} displayAlert={select} onClick={()=>setSelect(select => !select)}/>
-      <img src={props.image} alt="photo" className="photo-list__image"/>
+      <img src={image} alt="photo" className="photo-list__image"/>
       <div className="photo-list__user-details">
-        <img src={props.profile} alt="profile picture" className="photo-list__user-profile"/>
+        <img src={profile} alt="profile picture" className="photo-list__user-profile"/>
         <div className="photo-list__user-info">
-          <span>{props.username}</span>
+          <span>{username}</span>
           <br></br>
-          <span className="photo-list__user-location">{props.city}, {props.country}</span>
+          <span className="photo-list__user-location">{city}, {country}</span>
         </div>
       </div>
     </div>
